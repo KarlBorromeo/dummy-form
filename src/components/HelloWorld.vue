248d0ca76153a5b861829c1e30a396ba9d401733
KarlBorromeo <98805305+KarlBorromeo@users.noreply.github.com>
2023-09-27 00:04:51 +0800

    done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,69 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <form @submit.prevent="submit">
+    <section class="form-decor">
+      <label for="fullname">Fullname</label><input id="fullname" type="text" v-model="fullname">
+    </section>
+
+    <section class="form-decor">
+      <label for="occupation">Occupation</label><input type="text" id="occupation" v-model="occupation">
+    </section>
+
+    <section class="form-decor">
+      <h5>Sex</h5>
+      <label for="male">Male</label><input type="radio" id="male" value="male" v-model="sex">
+      <label for="female">Female</label><input type="radio" id="female" value="female" v-model="sex">
+    </section>
+
+    <section class="form-decor">
+      <label for="email">Email</label><input type="email" id="email" v-model="email">
+    </section>
+
+    <section class="form-decor">
+      <label for="age">Age</label><input type="number" id="age" v-model="age">
+    </section>
+
+    <button>Submit</button>
+
+  </form>
+
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return {
+      fullname: '',
+      occupation: '',
+      sex: '',
+      email: '',  
+      age: ''
+    }
+  },
+  methods: {
+    submit(){
+      const payload = {
+        fullname: this.fullname,
+        occupation: this.occupation,
+        sex: this.sex,
+        email: this.email,
+        age: this.age
+      }
+
+      console.log(payload)
+
+      //do request here
+      // try{
+      //   // request here
+      // }catch{
+      //   alert("something wrong")
+      // }
+    }
   }
+
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
